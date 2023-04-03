0c4644850fee913e73f511263574272458097fb5
Tohar_zioni <121717708+LibGPY@users.noreply.github.com>
2023-04-03 23:35:50 +0300

    the first scripts in here pick one to edit and after you editing it send it to us

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("OPEN UTARIX 1.0" + "\n");
+        System.out.println("Enter new username:");
+        String username;
+        username = input.nextLine();
+        System.out.println("username are set to:" + username);
+    }
+}
